5c0d941eb0fc04fcc703b56a7ac03342053aad28
Yuxuan Xie <teststaybaka@gmail.com>
2021-06-24 06:11:50 +0000

    Stringify args including errors properly.

--- puppeteer_executor.ts
+++ puppeteer_executor.ts
@@ -10,12 +10,12 @@ import { DELETE, EXIT, SCREENSHOT } from "@selfage/puppeteer_executor_api/cmds";
 let pipeline = util.promisify(stream.pipeline);
 
 let HOST_NAME = "localhost";
-let PATH_EXTRACTION_REGEX = /^\/(.*)$/;
 
 export interface OutputCollection {
   log: Array<string>;
   warn: Array<string>;
   error: Array<string>;
+  other: Array<string>;
 }
 
 export async function executeInPuppeteer(
@@ -50,9 +50,15 @@ export async function executeInPuppeteer(
 
   let browser = await puppeteer.launch();
   let page = await browser.newPage();
-  let outputCollection: OutputCollection = { log: [], warn: [], error: [] };
+  let outputCollection: OutputCollection = {
+    log: [],
+    warn: [],
+    error: [],
+    other: [],
+  };
   let executePromise = new Promise<void>((resolve) => {
     page.on("console", async (msg) => {
+      let output = await consoleMsgToString(msg);
       if (msg.type() === "log") {
         if (msg.text() === EXIT) {
           await shutDown(browser, server, tempBinFile);
@@ -67,30 +73,30 @@ export async function executeInPuppeteer(
           await fs.promises.unlink(file);
         } else {
           if (outputToConsole) {
-            console.log(msg.text());
+            console.log(output);
           }
-          outputCollection.log.push(msg.text());
+          outputCollection.log.push(output);
         }
       } else if (msg.type() === "info") {
         if (outputToConsole) {
-          console.info(msg.text());
+          console.info(output);
         }
-        outputCollection.log.push(msg.text());
+        outputCollection.log.push(output);
       } else if (msg.type() === "warning") {
         if (outputToConsole) {
-          console.warn(msg.text());
+          console.warn(output);
         }
-        outputCollection.warn.push(msg.text());
+        outputCollection.warn.push(output);
       } else if (msg.type() === "error") {
         if (outputToConsole) {
-          console.error(msg.text());
+          console.error(output);
         }
-        outputCollection.error.push(msg.text());
+        outputCollection.error.push(output);
       } else {
         if (outputToConsole) {
-          console.log(`${msg.type()}: ${msg.text()}`);
+          console.log(`${msg.type()}: ${output}`);
         }
-        outputCollection.log.push(msg.text());
+        outputCollection.other.push(`${msg.type()}: ${output}`);
       }
     });
     page.on("pageerror", async (err) => {
@@ -112,14 +118,33 @@ export async function executeInPuppeteer(
   return outputCollection;
 }
 
+async function consoleMsgToString(
+  msg: puppeteer.ConsoleMessage
+): Promise<string> {
+  if (msg.args().length > 0) {
+    let args = await Promise.all(
+      msg.args().map((arg) => {
+        return arg.executionContext().evaluate((arg: any) => {
+          if (arg instanceof Error) {
+            return arg.stack;
+          }
+          return `${arg}`;
+        }, arg);
+      })
+    );
+    return util.format(...args);
+  } else {
+    return msg.text();
+  }
+}
+
 async function serveFile(
   baseDir: string,
   request: http.IncomingMessage,
   response: http.ServerResponse
 ): Promise<void> {
   let url = new URL(request.url, `http://${request.headers.host}`);
-  let matched = url.pathname.match(PATH_EXTRACTION_REGEX);
-  let file = path.join(baseDir, matched[1]);
+  let file = path.join(baseDir, url.pathname.substring(1));
   let contentType = mime.lookup(path.extname(file));
   if (typeof contentType === "boolean") {
     throw new Error(`Unsupported file ext for ${file}.`);
